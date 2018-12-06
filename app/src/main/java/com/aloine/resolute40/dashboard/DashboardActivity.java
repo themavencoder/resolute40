a00cba1e75c6de9d1dfe7d74ccbe1d0ce6408b86
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-12-06 10:36:45 +0100

    Latitudes and Longitudes working with delay

--- app/src/main/java/com/aloine/resolute40/dashboard/DashboardActivity.java
+++ app/src/main/java/com/aloine/resolute40/dashboard/DashboardActivity.java
@@ -1,19 +1,72 @@
 package com.aloine.resolute40.dashboard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.GridLayout;
+import android.widget.Toast;
 
 import com.aloine.resolute40.R;
+import com.aloine.resolute40.map.MapsActivity;
+
 
 public class DashboardActivity extends AppCompatActivity {
+    private GridLayout gridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.dashboard);
         hideNavigationBar();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        init();
 
+       /* if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("");
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+*/
+
+    }
+
+    private void init() {
+        gridLayout = findViewById(R.id.mainGrid);
+        setSingleEvent(gridLayout);
+    }
+
+    private void setSingleEvent(GridLayout gridLayout) {
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            CardView cardView = (CardView) gridLayout.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch (finalI) {
+                        case 0:
+                            Toast.makeText(DashboardActivity.this, "Map your farm is clicked", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(DashboardActivity.this,MapsActivity.class));
+                            break;
+                        case 1:
+                            Toast.makeText(DashboardActivity.this, "Activate panic button", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 2:
+                            Toast.makeText(DashboardActivity.this, "View your map farm", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 3:
+                            Toast.makeText(DashboardActivity.this, "Vet Aid", Toast.LENGTH_SHORT).show();
+                            break;
+                        default:
+                    }
+
+                }
+            });
+        }
     }
 
     private void hideNavigationBar() {
---
+++ app/src/main/java/com/aloine/resolute40/map/MapsActivity.java
@@ -0,0 +1,106 @@
+package com.aloine.resolute40.map;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.aloine.resolute40.R;
+
+import im.delight.android.location.SimpleLocation;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private SimpleLocation location;
+    SimpleLocation.Point position;
+    Context context = this;
+    boolean requireFineGranularity = true;
+    boolean passiveMode = false;
+    long updateIntervalInMilliseconds = 2000;
+    boolean requireNewLocation = true;
+
+    private TextView latitude, longitude;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+
+        location = new SimpleLocation(context, requireFineGranularity, passiveMode, updateIntervalInMilliseconds, requireNewLocation);
+    // position =  location.getPosition();
+
+        init();
+
+        // if we can't access the location yet
+        if (!location.hasLocationEnabled()) {
+            // ask the user to enable location access
+            SimpleLocation.openSettings(this);
+        }
+
+
+    }
+
+    private void init() {
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+    }
+
+
+    public void getLocation(View view) {
+
+        final double lat = location.getLatitude();
+       final double longg = location.getLongitude();
+        latitude.setText(String.valueOf(lat));
+        longitude.setText(String.valueOf(longg));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        location.beginUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        location.endUpdates();
+        super.onPause();
+    }
+}
